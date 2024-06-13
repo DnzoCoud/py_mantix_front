e1d8ae09638561b18321988156076585e5404890
Dnzo <dangelsfocus@gmail.com>
2024-06-12 23:33:26 -0500

    Casi finalizado

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        HOST: process.env.HOST,
+        PORT: process.env.PORT,
+    },
+};
 
 export default nextConfig;
